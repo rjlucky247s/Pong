c71d43dc89aa99afff5ba6e6a0647f8ecec7ee74
rjlucky247s <rjlucky247s@gmail.com>
2018-03-13 15:20:53 -0400

    Project Creation

---
+++ Pong Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+    public float speed;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Translate(new Vector3(0,speed * Input.GetAxis("Vertical")*Time.deltaTime,0));
+	}
+}
